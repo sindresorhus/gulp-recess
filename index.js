ccf3b0a3122c68aeeef12da3046dcdbca51349f2
Guido Bouman <m@guido.vc>
2014-03-06 12:42:08 +0100

    Proper boolean casting.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ recess.Constructor.prototype.read = function () {
 module.exports = function (options) {
 	options = options || {};
 	
-	var logOnSuccess = options.logOnSuccess || true;
+	var logOnSuccess = !!options.logOnSuccess;
 	delete options.logOnSuccess;
 
 	return through.obj(function (file, enc, cb) {
